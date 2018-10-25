110bff8dad77bc0ef6dd607987a2d7b362adec2a
JakubRak <jakub.r.rak@gmail.com>
2018-10-26 00:01:38 +0200

    extractors and connectors

---
+++ jobs/tools/commons.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+
+class CommonData(object):
+
+    regex = {
+        "category": "/pol_m_(.*)-(\d+)\.html",
+        "category_pagination": "/pol_m_(.*)-(\d+)\.html\?counter=(\d+)",
+        "product": "/product-pol-(\d+)-(.*)\.html"
+        }
+
+    patterns = {
+        "homepage": "https://vubu.pl/",
+        "category": "https://vubu.pl/pol_m_%s-%d.html",
+        "category_pagination": "https://vubu.pl/pol_m_%s-%d.html?counter=%d",
+        "product": "https://vubu.pl/product-pol-%d-%s.html"
+    }
+
+    def soup_result(self, result):
+        return BeautifulSoup(result, features="html.parser")
+
+    # soup finder
+    def hyperlink_filter(self, tag, regex_string):
+        if tag.name != 'a':
+            return False
+        if not tag.has_attr('href'):
+            return False
+        if not tag['href']:
+            return False
+        href = tag['href']
+        regex = compile(regex_string)
+        if regex.match(href):
+            return True
+        return False
+        
--- jobs/tools/connector.py
+++ jobs/tools/connector.py
@@ -1,23 +1,12 @@
 from urllib.request import urlopen
 from re import compile
-from bs4 import BeautifulSoup
 
-class Connector(object):
+from jobs.tools.commons import CommonData
+
+class Connector(CommonData):
 
     protocole = "https://"
     domain = "vubu.pl"
-
-    regex = {
-        "category": "/pol_m_(.*)-(\d+)\.html",
-        "product": "/product-pol-(\d+)-(.*)\.html"
-        }
-
-    patterns = {
-        "homepage": "https://vubu.pl/",
-        "category": "https://vubu.pl/pol_m_%s-%d.html",
-        "category_pagination": "https://vubu.pl/pol_m_%s-%d.html?counter=%d",
-        "product": "https://vubu.pl/product-pol-%d-%s.html"
-    }
 
     # connection methods
     def correct_url(self, url):
@@ -34,55 +23,8 @@ class Connector(object):
         string = response.decode("utf-8")
         return string
 
-    def soup_result(self, result):
-        return BeautifulSoup(result, features="html.parser")
+    def pagination_hyperlink_filter(self, tag):
+        return self.hyperlink_filter(tag, self.regex['category_pagination'])
 
-    # soup finder
-    def category_hyperlink_filter(self, tag):
-        if tag.name != 'a':
-            return False
-        if not tag.has_attr('href'):
-            return False
-        if not tag['href']:
-            return False
-        href = tag['href']
-        regex = compile(self.regex['category'])
-        if regex.match(href):
-            return True
-        return False
-
-    # homepage processing
-    def extract_category_from_tag(self, tag):
-        # [URL, full_name_category, category_id, parent_categories_count]
-        # ['https://vubu.pl/pol_m_DLA-NIEJ_Odziez-damska-604.html', 'DLA-NIEJ_Odziez-damska', 604, 2]
-        href = tag['href']
-        regex = compile(self.regex['category'])
-        match = regex.match(href)
-        category_id = int(match.group(2))
-        category_full_name = match.group(1)
-        depth = len(category_full_name.split("_"))
-        proper_url = self.patterns['category'] % (match.group(1), category_id) # cleans GET parameters
-        return [proper_url, category_full_name, category_id, depth]
-
-    def find_all_categories_list(self):
-        connection_result = self.make_connection(self.patterns['homepage'])
-        soup = self.soup_result(connection_result)
-        found_tags = soup.find_all(self.category_hyperlink_filter)
-        tags_list = []
-        tags_ids = []
-        for tag in found_tags:
-            data = self.extract_category_from_tag(tag)
-            if data[2] in tags_ids:
-                continue
-            tags_ids.append(data[2])
-            tags_list.append(data)
-        return tags_list
-
-    def find_last_categories_list(self):
-        all_categories = self.find_all_categories_list()
-        result = []
-        for category in all_categories:
-            if category[2] >= 3:
-                result.append(category)
-        return result
-
+    def product_hyperlink_filter(self, tag):
+        return self.hyperlink_filter(tag, self.regex['product'])
