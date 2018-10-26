b509ce379831e07c97116ad7b41a8f5797eee76d
JakubRak <jakub.r.rak@gmail.com>
2018-10-26 23:52:35 +0200

    requirements and settings

--- vubuetl/settings.py
+++ vubuetl/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
     'jobs'
 ]
 
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from vubuetl.settings_local import *
+except Exception as e:
+    print(e)
