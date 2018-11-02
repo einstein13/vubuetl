6e2be20941b424310463ac9557852a9f2634b565
JakubRak <jakub.r.rak@gmail.com>
2018-11-02 23:45:10 +0100

    jobs admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -6,10 +6,13 @@ from .models import Session, Job
 
 
 class SessionAdmin(admin.ModelAdmin):
-    pass
+    list_display=('public_id', 'start_time', 'last_update', 'status', 'category_url')
 
 class JobAdmin(admin.ModelAdmin):
-    pass
+    list_display=('url', 'session_id', 'status', 'connection_errors')
+
+    def session_id(self, obj):
+        return "%d" % (obj.session.pk, )
 
 admin.site.register(Session, SessionAdmin)
 admin.site.register(Job, JobAdmin)
